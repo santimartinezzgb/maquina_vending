a818b18e79ec3d49c263805090a9dded99098c56
smz7z <santimgb98@gmail.com>
2025-11-26 19:54:00 +0100

    Actualizar

--- src/main.mjs
+++ src/main.mjs
@@ -3,14 +3,15 @@ import { app, BrowserWindow } from 'electron';
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1700,
-        height: 1000,
         minWidth: 1700,
-        minHeight: 1000,
         maxWidth: 1700,
+
+        height: 1000,
+        minHeight: 1000,
         maxHeight: 1000
     });
 
-    win.loadFile('ventana_principal.html');
+    win.loadFile('src/ventana_principal.html');
 }
 
 app.whenReady().then(createWindow);
